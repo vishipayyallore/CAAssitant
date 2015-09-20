9b52599976b2379f86e296895dc87889d4b10524
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-20 12:14:01 +0530

    Initial Version

---
+++ CAAssistant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAAssistant.MvcApplication" Language="C#" %>
---
+++ CAAssistant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAAssistant.Startup))]
+namespace CAAssistant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
